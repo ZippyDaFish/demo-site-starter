1a1da5bc1be0455795e2144e2d3ea606854488e0
Andrew Riker <andrew.riker21@gmail.com>
2025-12-12 13:22:23 -0500

    Updates

---
+++ _app/immutable/chunks/CL2dvN_z.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1k5733t?.base??"/demo-site-starter",t=globalThis.__sveltekit_1k5733t?.assets??s??"";export{t as a,s as b};
--- _app/immutable/chunks/DJfnsbOK.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_14r1by0?.base??"/demo-site-starter",e=globalThis.__sveltekit_14r1by0?.assets??s??"";export{e as a,s as b};
